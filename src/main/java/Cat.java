1860f81704c8a0890c6ff0d653dfe2f2b586c277
Nikita3141 <nikita.ingilizyan@gmail.com>
2023-10-26 19:43:14 +0300

    Test3

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -2,7 +2,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
-@Configuration
+
 
 
 
